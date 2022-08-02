cbb2d033015481b7c72f40439c02f9b6efdc0203
aps08 <anoopprsingh.social@gmail.com>
2022-08-02 10:05:23 +0530

    new structure

---
+++ src/email_service.py
@@ -0,0 +1,13 @@
+"""
+Module string
+"""
+import sys
+
+sys.dont_write_bytecode = True
+
+
+class EmailService:
+    """read and sends email"""
+
+    def __init__(self) -> None:
+        pass
---
+++ src/main.py
@@ -0,0 +1,5 @@
+"""doc string"""
+
+
+if __name__ == "__main__":
+    print("run")
